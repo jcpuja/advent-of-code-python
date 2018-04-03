e7b29876cee4d874663ac49deeac696e06d22d9e
Jean-Christophe Puja <jc.puja@gmail.com>
2018-04-03 18:11:28 +0200

    Day 5 Task 1 done

---
+++ day05/task01.py
@@ -0,0 +1,13 @@
+with open('input.txt') as f:
+    inputs = [int(s) for s in list(f)]
+
+    current_index = 0
+    steps = 0
+
+    while 0 <= current_index < len(inputs):
+        steps += 1
+        this_offset = inputs[current_index]
+        inputs[current_index] = this_offset + 1
+        current_index += this_offset
+
+    print(steps)
