83ee859de7a7d8b7b399a2e16bdfa89fc8dd2546
Jean-Christophe Puja <jc.puja@gmail.com>
2018-05-04 18:43:04 +0200

    Day 10 task 1: improved failing test

--- day10/task01.py
+++ day10/task01.py
@@ -5,4 +5,16 @@ class KnotHash:
         self.lengths = lengths
         self.list = list(range(list_size))
 
+    def get_current_position(self):
+        return self.current_position
+
+    def get_list(self):
+        return self.list
+
+    def get_skip_size(self):
+        return self.skip_size
+
+    def compute_checksum(self):
+        current_list = self.get_list()
+        return current_list[0] * current_list[1]
 
--- day10/task01tests.py
+++ day10/task01tests.py
@@ -5,8 +5,12 @@ from day10.task01 import KnotHash
 
 class TestKnotHash(unittest.TestCase):
 
+    @unittest.skip("Acceptance test, will not pass until implementation is complete")
     def testExampleScenario(self):
         sut = KnotHash([3, 4, 1, 5], list_size=5)
-        self.assertEqual(4, sut.skip_size)
-        self.assertEqual(4, sut.current_position)
-        self.assertEqual([3, 4, 2, 1, 0], sut.list)
+        self.assertEqual(4, sut.get_skip_size())
+        self.assertEqual(4, sut.get_current_position())
+        self.assertEqual([3, 4, 2, 1, 0], sut.get_list())
+        self.assertEqual(12, sut.compute_checksum())
+
+
