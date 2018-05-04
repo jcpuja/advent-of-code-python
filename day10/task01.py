f59ad561bf3fc118669edf57e1549be706bb6dae
Jean-Christophe Puja <jc.puja@gmail.com>
2018-05-04 20:54:52 +0200

    Day 10 task 1: CircularList class, reverse operation done

--- day10/task01.py
+++ day10/task01.py
@@ -1,15 +1,54 @@
+class CircularList:
+    def __init__(self, list_size=256):
+        self.list = list(range(list_size))
+
+    def get_list(self):
+        return self.list
+
+    def reverse(self, position, length):
+        list_length = len(self.list)
+
+        if not 0 <= position < list_length:
+            raise Exception('Start position for reverse is out of bounds: list_length='
+                            + str(list_length) + ', position=' + str(position))
+
+        if not 1 <= length <= list_length:
+            raise Exception('Slice length for reverse is out of bounds: list_length='
+                            + str(list_length) + ', length=' + str(length))
+
+        end_index = position + length
+
+        if end_index <= list_length:
+            slice_to_reverse = self.list[position:end_index]
+            slice_to_reverse.reverse()
+            self.list[position:end_index] = slice_to_reverse
+
+        else:
+            actual_end_index = int(end_index % list_length)
+
+            right_slice = self.list[position:]
+            left_slice = self.list[:actual_end_index]
+
+            slice_to_reverse = right_slice + left_slice
+            slice_to_reverse.reverse()
+
+            self.list[position:] = slice_to_reverse[:len(right_slice)]
+            self.list[:actual_end_index] = slice_to_reverse[len(left_slice):]
+
+
+
 class KnotHash:
     def __init__(self, lengths, list_size=256):
         self.current_position = 0
         self.skip_size = 0
         self.lengths = lengths
-        self.list = list(range(list_size))
+        self.circular_list = CircularList(list_size)
 
     def get_current_position(self):
         return self.current_position
 
     def get_list(self):
-        return self.list
+        return self.circular_list.get_list()
 
     def get_skip_size(self):
         return self.skip_size
--- day10/task01tests.py
+++ day10/task01tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day10.task01 import KnotHash
+from day10.task01 import KnotHash, CircularList
 
 
 class TestKnotHash(unittest.TestCase):
@@ -14,3 +14,44 @@ class TestKnotHash(unittest.TestCase):
         self.assertEqual(12, sut.compute_checksum())
 
 
+class TestCircularList(unittest.TestCase):
+
+    def testInitialization(self):
+        sut = CircularList(list_size=5)
+        self.assertEqual([0, 1, 2, 3, 4], sut.get_list())
+
+    def testReverse_whenPositionPlusLengthEqualsListLength_shouldReverseFromPositionToListEnd(self):
+        sut = CircularList(5)
+        sut.reverse(1, 4)
+        self.assertEqual([0, 4, 3, 2, 1], sut.get_list())
+
+    def testReverse_whenPositionPlusLengthLTListLength_shouldReverseNoWrap(self):
+        sut = CircularList(5)
+        sut.reverse(1, 3)
+        self.assertEqual([0, 3, 2, 1, 4], sut.get_list())
+
+    def testReverse_whenPositionPlusLengthGTListLength_shouldReverseWithWrap(self):
+        sut = CircularList(5)
+        sut.reverse(4, 2)
+        self.assertEqual([4, 1, 2, 3, 0], sut.get_list())
+
+    def testReverse_whenLengthEquals1_shouldNotChangeList(self):
+        sut = CircularList(5)
+        sut.reverse(position=0, length=1)
+        self.assertEqual([0, 1, 2, 3, 4], sut.get_list())
+
+    def testReverse_whenPositionGTEListLength_shouldRaise(self):
+        sut = CircularList(5)
+        self.assertRaises(Exception, sut.reverse, 5, 1)
+
+    def testReverse_whenPositionLT0_shouldRaise(self):
+        sut = CircularList(5)
+        self.assertRaises(Exception, sut.reverse, -1, 1)
+
+    def testReverse_whenLengthGTListLength_shouldRaise(self):
+        sut = CircularList(5)
+        self.assertRaises(Exception, sut.reverse, 0, 6)
+
+    def testReverse_whenLengthLT1_shouldRaise(self):
+        sut = CircularList(5)
+        self.assertRaises(Exception, sut.reverse, 0, 0)
