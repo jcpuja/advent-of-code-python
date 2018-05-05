93aafd06097fe01f3dc0ffb8bf0b6d08f16ad64e
Jean-Christophe Puja <jc.puja@gmail.com>
2018-05-05 14:10:25 +0200

    Day 10 task 2: initial position and skip size are parameters

--- day10/knot_hash_round.py
+++ day10/knot_hash_round.py
@@ -2,18 +2,18 @@ from day10.circular_list import CircularList
 
 
 class KnotHashRound:
-    def __init__(self, lengths, list_size=256):
-        self.current_position = 0
-        self.skip_size = 0
+    def __init__(self, lengths, list_size=256, initial_position=0, initial_skip_size=0):
+        self.position = initial_position
+        self.skip_size = initial_skip_size
         self.list = CircularList(list_size)
 
         for length in lengths:
-            self.list.reverse(self.current_position, length)
-            self.current_position = self.list.get_actual_position(self.current_position + length + self.skip_size)
+            self.list.reverse(self.position, length)
+            self.position = self.list.get_actual_position(self.position + length + self.skip_size)
             self.skip_size += 1
 
     def get_current_position(self):
-        return self.current_position
+        return self.position
 
     def get_list(self):
         return self.list.get_list()
