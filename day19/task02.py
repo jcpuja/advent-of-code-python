fdb5bdbe8a570f684f73958a0f3fa68071c0fbce
Jean-Christophe Puja <jc.puja@gmail.com>
2018-06-22 15:45:43 +0200

    Day 19 task 2 done

---
+++ day19/task02.py
@@ -0,0 +1,90 @@
+PIPE = '|'
+DASH = '-'
+PLUS = '+'
+EMPTY = ' '
+N = 'north'
+S = 'south'
+E = 'east'
+W = 'west'
+dirs = [N, S, E, W]
+letters = []
+
+travel_deltas = {
+    N: (0, -1),
+    S: (0, 1),
+    E: (1, 0),
+    W: (-1, 0),
+}
+
+grid = []
+start_x = None
+
+# input_file = 'example_input.txt'
+input_file = 'input.txt'
+with open(input_file) as f:
+    for yi, line in enumerate(f):
+        grid.append([])
+        for xi, char in enumerate(line.rstrip()):
+            grid[yi].append(char)
+            if not start_x and char == PIPE:
+                start_x = xi
+
+cursor = start_x, 0
+travel_direction = S
+step_counter = 0
+
+
+def safe_get(x, y):
+    try:
+        c = grid[y][x]
+    except IndexError:
+        c = EMPTY
+    return c
+
+
+def travel(direction, c):
+    x, y = c
+    dx, dy = travel_deltas[direction]
+    return x + dx, y + dy
+
+
+def process(direction, cur):
+    x, y = cur
+    ch = safe_get(x, y)
+
+    new_direction = direction
+
+    if ch == EMPTY:
+        # We ran out of road, interpret this as "done"
+        return True, None
+
+    elif ch == PIPE or ch == DASH:
+        # Simple road, nothing special to do
+        pass
+
+    elif ch == PLUS:
+        # Direction change, detect new direction
+        to_check = [N, S] if direction in (E, W) else [E, W]
+        for d in to_check:
+            test_x, test_y = travel(d, cur)
+            if safe_get(test_x, test_y) != EMPTY:
+                new_direction = d
+
+    else:
+        # A letter, store it in our list
+        letters.append(ch)
+
+    return False, new_direction
+
+
+# print(cursor)
+
+while True:
+    cursor = travel(travel_direction, cursor)
+    # print(cursor)
+    done, travel_direction = process(travel_direction, cursor)
+    step_counter += 1
+    if done:
+        break
+
+print(step_counter)
