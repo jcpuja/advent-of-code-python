b98cadc412eeb19cb7b82b409f50dc537bf9268c
Jean-Christophe Puja <jc.puja@gmail.com>
2018-07-06 15:12:13 +0200

    Day 21 task 1 new try - rule build done

--- day21/task01.py
+++ day21/task01.py
@@ -1,42 +1,44 @@
-from day21.grid import Grid
+import numpy as np
 
-rules = {}
 
-filename = 'example_input.txt'
-iterations = 2
-# filename = 'input.txt'
-# iterations = 5
-with open(filename) as f:
-    for line in [l.rstrip() for l in f]:
-        src, tgt = line.split(' => ')
-        rules[src] = tgt
+def enhance(flat_in, rules):
+    pass
 
-g = Grid.initial()
 
-for i in range(iterations):
-    sub_grids = g.split()  # 2-d array of grids
-    new_grids = [[None] * len(sub_grids)] * len(sub_grids)  # Empty 2-d array of the same size
-    for y, row in enumerate(sub_grids):
-        for x, sg in enumerate(row):
-            for pattern in sg.matching_patterns:
-                if pattern in rules:
-                    new_grids[y][x] = Grid.from_pattern(rules[pattern])
-                    break
+def combinations(in_arr):
+    combs = set()
+    for k in range(4):
+        combs.add(grid_as_string(np.rot90(in_arr, k)))
+        combs.add(grid_as_string(np.fliplr(np.rot90(in_arr, k))))
+        combs.add(grid_as_string(np.flipud(np.rot90(in_arr, k))))
 
-    merged_grid = None
-    for y, row in enumerate(new_grids):
-        merged_row = None
-        for x, grid_part in enumerate(row):
-            if not merged_row:
-                merged_row = grid_part
+    return combs
+
+
+def grid_as_string(arr):
+    return ''.join([str(c) for c in arr.flatten()])
+
+
+def build_rules(example):
+    file_name = 'example_input.txt' if example else 'input.txt'
+    rules2 = {}
+    rules3 = {}
+    with open(file_name) as f:
+        for l in f:
+            src, tgt = l.strip().split(' => ')
+            src = src.replace('/', '')
+            tgt = tgt.replace('/', '')
+            src = np.array([1 if c == '#' else 0 for c in src])
+            tgt = np.array([1 if c == '#' else 0 for c in tgt])
+
+            if len(src) == 4:
+                for src_combination in combinations(src.reshape(2, 2)):
+                    rules2[src_combination] = tgt.reshape(3, 3)
+            elif len(src) == 9:
+                for src_combination in combinations(src.reshape(3, 3)):
+                    rules3[src_combination] = tgt.reshape(4, 4)
+
             else:
-                merged_row = merged_row.concat(grid_part, 0)
+                assert False
 
-        if not merged_grid:
-            merged_grid = merged_row
-        else:
-            merged_grid = merged_grid.concat(merged_row, 1)
-
-    g = merged_grid
-
-print(g.count_active())
+    return rules2, rules3
---
+++ day21/tests.py
@@ -0,0 +1,34 @@
+import numpy as np
+import unittest
+from unittest import TestCase
+
+from day21.task01 import enhance, build_rules
+
+
+class GridUnitTests(TestCase):
+
+    # Acceptance tests
+
+    @unittest.skip('Acceptance test')
+    def test_enhance_example_step1(self):
+        rules = build_rules(example=True)
+        self.assertEqual('1001000000001001', enhance('010001111', rules))
+
+    @unittest.skip('Acceptance test')
+    def test_enhance_example_step2(self):
+        rules = build_rules(example=True)
+        self.assertEqual('110110100100000000110110100100000000', enhance('1001000000001001', rules))
+
+    # Unit tests
+
+    def test_build_rules(self):
+        rules2, rules3 = build_rules(example=True)
+
+        self.assertEqual(4, len(rules2))
+        example_post = np.array([int(i) for i in list('110100000')]).reshape(3, 3)
+        self.assertTrue(np.array_equal(example_post, rules2['0001']))
+        self.assertTrue(np.array_equal(example_post, rules2['0100']))
+        self.assertTrue(np.array_equal(example_post, rules2['0010']))
+        self.assertTrue(np.array_equal(example_post, rules2['0010']))
+
+        self.assertEqual(8, len(rules3))
